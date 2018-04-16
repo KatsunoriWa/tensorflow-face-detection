647717c4a1a464a3246e5f5545528d11678bb339
papa <gtx285aa@yahoo.co.jp>
2018-04-16 23:50:21 +0900

    used optional argument scale

--- tfFaceDetector.py
+++ tfFaceDetector.py
@@ -17,47 +17,7 @@ from utils import visualization_utils_color as vis_util
 
 import readheadPose
 
-def rotate(img, deg):
-    """rotate anti-clockwise
-    img: numpy image
-    deg:
-    """
-    pilImg = PIL.Image.fromarray(np.uint8(img))
-    rotated = pilImg.rotate(deg)
-    return np.asarray(rotated)+0
-
-def isInside(point, leftTop, rightBottom):
-    """
-    return True if point is in the rectangle define by leftTop and rightBottom
-    """
-
-    if not (leftTop[0] < point[0] < rightBottom[0]):
-        return False
-    if not (leftTop[1] < point[1] < rightBottom[1]):
-        return False
-    return True
-
-def centerIsInRect(shape, leftTop, rightBottom):
-    center = (shape[1]/2, shape[0]/2)
-    return isInside(center, leftTop, rightBottom)
-
-def scaledImage(img, scale, keepFullSize=True):
-    """
-    return resized image
-    """
-
-    [h, w] = img.shape[:2]
-    scaledSize = (int(round(w*scale)), int(round(h*scale)))
-
-    scaledImg = cv.resize(img, scaledSize)
-
-    if not keepFullSize:
-        return scaledImg
-    else:
-        newImg = np.zeros((img.shape), dtype=np.uint8)
-        newImg[0:scaledSize[1], 0:scaledSize[0], :] = scaledImg
-        return newImg
-
+from helper import *
 
 def getGategoryIndex():
     # List of the strings that is used to add correct label for each box.
@@ -141,8 +101,8 @@ def processDatabase(dataset, names, deg=0, scale=1.0, min_score_thresh=0.7, show
         d = readheadPose.getTruePosition()
 
 
-    log = open("log_%s_%d.csv" % (dataset, deg), "wt")
-    log.write("name,num,truePositives,falsePositives\n")
+    log = open("log_%s_%d_%f.csv" % (dataset, deg, scale), "wt")
+    log.write("name,num,truePositives,falsePositives,meanSize\n")
 
     tDetector = TensoflowFaceDector()
     category_index = getGategoryIndex()
@@ -187,6 +147,8 @@ def processDatabase(dataset, names, deg=0, scale=1.0, min_score_thresh=0.7, show
 
         if dataset in ("lfw", ):
             center = imgCenter
+            center = (int(scale*center[0]), int(scale*center[1]))
+
         elif dataset == "headPose":
             v = d[p]
             center = (v[0], v[1])
@@ -199,13 +161,16 @@ def processDatabase(dataset, names, deg=0, scale=1.0, min_score_thresh=0.7, show
             cv.circle(frame, center, r, (0, 255, 0))
         else:
             center = imgCenter
+            center = (int(scale*center[0]), int(scale*center[1]))
 
 
+        trueSizes = []
         boxes_shape = boxes.shape
         for i in range(boxes_shape[0]):
             ymin, xmin, ymax, xmax = boxes[i, 0], boxes[i, 1], boxes[i, 2], boxes[i, 3]
             yLeftTop, xLeftTop, yRightBottom, xRightBottom = ymin * h, xmin * w, ymax * h, xmax * w
             yLeftTop, xLeftTop, yRightBottom, xRightBottom = int(yLeftTop), int(xLeftTop), int(yRightBottom), int(xRightBottom)
+            width = xRightBottom - xLeftTop
 
             if scores[i] <= min_score_thresh:
                 continue
@@ -213,6 +178,7 @@ def processDatabase(dataset, names, deg=0, scale=1.0, min_score_thresh=0.7, show
             isPositive = isInside(center, (xLeftTop, yLeftTop), (xRightBottom, yRightBottom))
 
             trueDetection[isPositive] += 1
+            trueSizes.append(width)
 
             cv.circle(frame, (xLeftTop, yLeftTop), 5, (0, 255, 0))
             cv.circle(frame, (xRightBottom, yRightBottom), 5, (0, 255, 0))
@@ -222,7 +188,7 @@ def processDatabase(dataset, names, deg=0, scale=1.0, min_score_thresh=0.7, show
                          color, 5)
 
         found = trueDetection[True] + trueDetection[False]
-        log.write("%s, %d, %d, %d\n" % (p, found, trueDetection[True], trueDetection[False]))
+        log.write("%s, %d, %d, %d, %s\n" % (p, found, trueDetection[True], trueDetection[False],`np.mean(trueSizes)`))
 
         if windowNotSet is True:
             cv.namedWindow("tensorflow based (%d, %d)" % (w, h), cv.WINDOW_NORMAL)
